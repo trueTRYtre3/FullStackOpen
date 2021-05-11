6a8593fe8107fbc2ceb86e45eb1055fef7c3d17f
Kyle Ortega <kyleortega253@gmail.com>
2021-05-11 00:32:13 -0700

    implemented useeffect custom hook

--- Part7/country-hook/src/App.js
+++ Part7/country-hook/src/App.js
@@ -16,9 +16,16 @@ const useField = (type) => {
 }
 
 const useCountry = (name) => {
-  const [country, setCountry] = useState(null)
+  const [country, setCountry] = useState(null)  
 
-  useEffect(() => {})
+  useEffect(() => {
+    if (name.length !== 0) {
+      axios
+        .get(`https://restcountries.eu/rest/v2/name/${name}?fullText=true`)
+        .then(response => setCountry(response))
+        .catch(() => setCountry('no result'))
+    }
+  }, [name])
 
   return country
 }
@@ -28,7 +35,7 @@ const Country = ({ country }) => {
     return null
   }
 
-  if (!country.found) {
+  if (country === 'no result') {
     return (
       <div>
         not found...
@@ -38,10 +45,10 @@ const Country = ({ country }) => {
 
   return (
     <div>
-      <h3>{country.data.name} </h3>
-      <div>capital {country.data.capital} </div>
-      <div>population {country.data.population}</div> 
-      <img src={country.data.flag} height='100' alt={`flag of ${country.data.name}`}/>  
+      <h3>{country.data[0].name} </h3>
+      <div>capital {country.data[0].capital} </div>
+      <div>population {country.data[0].population}</div> 
+      <img src={country.data[0].flag} height='100' alt={`flag of ${country.data.name}`}/>  
     </div>
   )
 }
