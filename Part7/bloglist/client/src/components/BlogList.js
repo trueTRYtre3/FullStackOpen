1dd260468a5897b43f44167a2b5942ff0c58678a
Kyle Ortega <kyleortega253@gmail.com>
2021-05-29 23:52:48 -0700

    removing comments

--- Part7/bloglist/client/src/components/BlogList.js
+++ Part7/bloglist/client/src/components/BlogList.js
@@ -4,15 +4,6 @@ import { Card } from 'react-bootstrap'
 
 const BlogList = ({ children }) => {
 
-  // const blogStyle = {
-  //   paddingTop: 10,
-  //   paddingBottom: 10,
-  //   paddingLeft: 10,
-  //   border: 'solid',
-  //   borderWidth: 1,
-  //   marginBottom: 5
-  // }
-
   return (
     // <div style={blogStyle} className='blog'>
     <Card className='blog' style={{ width: '60%', margin: 'auto' }}>
