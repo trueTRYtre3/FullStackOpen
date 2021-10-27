c5df70a6624b1115a9dad8dad0f6c1bb307af8a7
Kyle Ortega <kyleortega253@gmail.com>
2021-10-26 18:31:54 -0700

    mongodb add

--- Part8/library-backend/index.js
+++ Part8/library-backend/index.js
@@ -3,122 +3,21 @@ const { v4: uuidv4 } = require('uuid')
 const jwt = require('jsonwebtoken')
 
 const mongoose = require('mongoose')
+const book = require('./models/book')
+const author = require('./models/author')
 
-let authors = [
-  {
-    name: 'Robert Martin',
-    id: "afa51ab0-344d-11e9-a414-719c6709cf3e",
-    born: 1952,
-  },
-  {
-    name: 'Martin Fowler',
-    id: "afa5b6f0-344d-11e9-a414-719c6709cf3e",
-    born: 1963
-  },
-  {
-    name: 'Fyodor Dostoevsky',
-    id: "afa5b6f1-344d-11e9-a414-719c6709cf3e",
-    born: 1821
-  },
-  { 
-    name: 'Joshua Kerievsky', // birthyear not known
-    id: "afa5b6f2-344d-11e9-a414-719c6709cf3e",
-  },
-  { 
-    name: 'Sandi Metz', // birthyear not known
-    id: "afa5b6f3-344d-11e9-a414-719c6709cf3e",
-  },
-]
+let { authors, books } = require('./data/fake_data')
+const typeDefs = require('./data/fake_data')
 
-let books = [
-  {
-    title: 'Clean Code',
-    published: 2008,
-    author: 'Robert Martin',
-    id: "afa5b6f4-344d-11e9-a414-719c6709cf3e",
-    genres: ['refactoring']
-  },
-  {
-    title: 'Agile software development',
-    published: 2002,
-    author: 'Robert Martin',
-    id: "afa5b6f5-344d-11e9-a414-719c6709cf3e",
-    genres: ['agile', 'patterns', 'design']
-  },
-  {
-    title: 'Refactoring, edition 2',
-    published: 2018,
-    author: 'Martin Fowler',
-    id: "afa5de00-344d-11e9-a414-719c6709cf3e",
-    genres: ['refactoring']
-  },
-  {
-    title: 'Refactoring to patterns',
-    published: 2008,
-    author: 'Joshua Kerievsky',
-    id: "afa5de01-344d-11e9-a414-719c6709cf3e",
-    genres: ['refactoring', 'patterns']
-  },  
-  {
-    title: 'Practical Object-Oriented Design, An Agile Primer Using Ruby',
-    published: 2012,
-    author: 'Sandi Metz',
-    id: "afa5de02-344d-11e9-a414-719c6709cf3e",
-    genres: ['refactoring', 'design']
-  },
-  {
-    title: 'Crime and punishment',
-    published: 1866,
-    author: 'Fyodor Dostoevsky',
-    id: "afa5de03-344d-11e9-a414-719c6709cf3e",
-    genres: ['classic', 'crime']
-  },
-  {
-    title: 'The Demon ',
-    published: 1872,
-    author: 'Fyodor Dostoevsky',
-    id: "afa5de04-344d-11e9-a414-719c6709cf3e",
-    genres: ['classic', 'revolution']
-  },
-]
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+  useCreateIndex: true
+})
+  .then(() => console.log('connected to MongoDB '))
+  .catch((error) => console.log('error connecting to MongoDB:', error.message))
 
-
-const typeDefs = gql`
-    type Author {
-        name: String!
-        bookCount: Int
-        id: ID!
-        born: Int
-    }
-
-    type Book {
-        title: String!
-        author: String!
-        published: Int!
-        genres: [String]
-        id: ID!
-    }
-
-    type Query {
-        bookCount: Int!
-        authorCount: Int!
-        allBooks(author: String, genre: String): [Book!]!
-        allAuthors: [Author!]!
-    }
-
-    type Mutation {
-        addBook(
-            title: String!
-            author: String!
-            published: Int!
-            genres: [String]
-        ): Book
-        editAuthor(
-            name: String!
-            born: Int!
-        ): Author
-    }
-`
 
 const resolvers = {
   Query: {
@@ -162,9 +61,7 @@ const resolvers = {
         },
         editAuthor: (root, args) => {
             let indexFound = authors.findIndex(author => author.name === args.name)
-            if (indexFound === -1) {
-                return null
-            }
+            if (indexFound === -1) return null
 
             const editedAuthor = {
                 name: args.name,
