23e23770518e52ef10e3eadf01e47c18da33e1bd
Kyle Ortega <kyleortega253@gmail.com>
2021-10-28 20:23:15 -0700

    models updated

--- Part8/library-backend/models/author.js
+++ Part8/library-backend/models/author.js
@@ -1,5 +1,4 @@
 const mongoose = require('mongoose')
-const uniqueValidator = require('mongoose-unique-validator')
 
 const schema = new mongoose.Schema({
   name: {
@@ -17,5 +16,4 @@ const schema = new mongoose.Schema({
   },
 })
 
-schema.plugin(uniqueValidator)
 module.exports = mongoose.model('Author', schema)
