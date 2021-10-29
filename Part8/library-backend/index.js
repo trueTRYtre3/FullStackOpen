5f9b9ce949f21b97951818992176f5c20501537d
Kyle Ortega <kyleortega253@gmail.com>
2021-10-28 20:29:18 -0700

    saving data to mongodb

--- Part8/library-backend/index.js
+++ Part8/library-backend/index.js
@@ -13,7 +13,7 @@ mongoose.connect(process.env.MONGODB_URI)
 
 const typeDefs = gql`
   type Author {
-      name: String
+      name: String!
       bookCount: Int
       id: ID
       born: Int
@@ -50,77 +50,106 @@ const typeDefs = gql`
 
 const resolvers = {
   Query: {
-      bookCount: () => Book.collection.countDocuments(),
-      authorCount: () => Author.collection.countDocuments(),
-      allBooks: (root, args) => {
-          if (!args.author && !args.genre) {
-              return books
-          }
-          let filteredBooks = books
+    bookCount: () => Book.collection.countDocuments(),
+    authorCount: () => Author.collection.countDocuments(),
+    allBooks: (root, args) => {
+        if (!args.author && !args.genre) {
+            return books
+        }
+        let filteredBooks = books
 
-          //   filter for author books
-          filteredBooks = args.author ? filteredBooks.filter(book => book.author === args.author) : filteredBooks
+        //   filter for author books
+        filteredBooks = args.author 
+          ? filteredBooks.filter(book => book.author === args.author) 
+          : filteredBooks
 
-          // filter for genres
-          filteredBooks = args.genre ? filteredBooks.filter(book => book.genres.includes(args.genre)) : filteredBooks
+        // filter for genres
+        filteredBooks = args.genre 
+          ? filteredBooks.filter(book => book.genres.includes(args.genre)) 
+          : filteredBooks
 
-          return filteredBooks
-      },
-      allAuthors: () => Author.find({}) 
+        return filteredBooks
     },
-    Author: {
-        bookCount: (root) => {
-          const books = Book.find( { author: { $in: root.name } } )
-          return books.length
-          // const author = books.filter(book => book.author === root.name)
-          // return author.length
-        }
-    },
-    Mutation: {
-      addBook: async (root, args, context) => {
-        const book = new Book({ ...args })
-        const currentUser = context.currentUser
-
-        if (!currentUser) {
-          throw new AuthenticationError('not authenticated')
-        }
-
-        try {
-          await book.save()
-
-        } catch (error) {
-          throw new UserInputError(error.message, {
-            invalidArgs: args,
-          })
-        }
-
-      },
-        // addBook: (root, args) => {
-        //     const id  = uuidv4()
-        //     const book = { ...args, id }
-        //     books = books.concat(book)
-        //     authors = authors.some(author => author.name === args.author) 
-        //         ? authors 
-        //         : authors.concat({
-        //             name: args.author, 
-        //             born: null,
-        //             id 
-        //         })
-        //     return book
-        // },
-        editAuthor: (root, args) => {
-            let indexFound = authors.findIndex(author => author.name === args.name)
-            if (indexFound === -1) return null
-
-            const editedAuthor = {
-                name: args.name,
-                born: args.born
-            }
-            authors[indexFound] = editedAuthor
-            return editedAuthor
-            
-        }
+    allAuthors: async () => 
+      await Author.find({})
+  },
+  Author: {
+    bookCount: (root) => {
+      const books = Book.find( { author: { $in: root.name } } )
+      return books.length
+      // const author = books.filter(book => book.author === root.name)
+      // return author.length
     }
+  },
+  Mutation: {
+    addBook: async (root, args) => {
+      let author = await Author.findOne({ name: args.author })
+      if (!author) {
+        author = new Author({
+          name: args.author
+        })
+      } 
+      author.bookCount+=1
+      try {
+        await author.save()
+      } catch(error) {
+        throw new UserInputError("Can't save Author")
+      }
+
+      const book = new Book({ 
+        ...args,  
+        author
+      })
+      try {
+        await book.save()
+      } catch (error) {
+        throw new UserInputError("Can't save book")
+      }
+      return book
+    },
+    editAuthor: async (root, args, context) => {
+      const authorExist = await Author.findOne({ name: args.name })
+      if (!authorExist) {
+        return null
+      }
+      let newAuthor = null
+      try {
+        newAuthor = await Author.findOneAndUpdate(
+          { name: args.name },
+          { born: args.born },
+          { new : true }
+        )
+      } catch (error) {
+        throw new UserInputError("Unable to edit author")
+      }
+      return newAuthor
+    }
+      // addBook: (root, args) => {
+      //     const id  = uuidv4()
+      //     const book = { ...args, id }
+      //     books = books.concat(book)
+      //     authors = authors.some(author => author.name === args.author) 
+      //         ? authors 
+      //         : authors.concat({
+      //             name: args.author, 
+      //             born: null,
+      //             id 
+      //         })
+      //     return book
+      // },
+    // editAuthor: (root, args) => {
+    //   let indexFound = authors.findIndex(author => author.name === args.name)
+    //   if (indexFound === -1) return null
+
+    //   const editedAuthor = {
+    //       name: args.name,
+    //       born: args.born
+    //   }
+    //   authors[indexFound] = editedAuthor
+    //   return editedAuthor
+        
+    // }
+  }
 
 }
 
