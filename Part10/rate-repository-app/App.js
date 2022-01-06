656b3c5f5c0de7a93581b88f0fc375ad9a169720
Kyle Ortega <kyleortega253@gmail.com>
2022-01-05 16:04:55 -0800

    new native started

--- Part10/rate-repository-app/App.js
+++ Part10/rate-repository-app/App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your new stuff!</Text>
+      <Text>Open up App.js to start working on your mobile stuff!</Text>
       <StatusBar style="auto" />
     </View>
   );
