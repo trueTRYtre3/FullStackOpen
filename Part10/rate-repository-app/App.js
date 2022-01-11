71863dfceeb0d03739e172452f47111d05fd30b5
Kyle Ortega <kyleortega253@gmail.com>
2022-01-10 18:43:29 -0800

    list created and styled on

--- Part10/rate-repository-app/App.js
+++ Part10/rate-repository-app/App.js
@@ -1,15 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
+import RepositoryList from './src/components/RepositoryList';
 import { StyleSheet, Text, View } from 'react-native';
-
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your mobile stuff!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
 
 const styles = StyleSheet.create({
   container: {
@@ -19,3 +11,14 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+const App = () => {
+  return (
+    <View style={styles.container}>
+      <RepositoryList />
+    </View>
+  );
+};
+
+export default App;
+
